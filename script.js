80f423b52b9a1d68582b4db6e9452940c3f844c5
Irlamfernandes <lanzinhofernandes@gmail.com>
2024-05-19 20:38:47 -0300

    Update

---
+++ script.js
@@ -0,0 +1,18 @@
+let currentTimezone = 'UTC';
+
+function updateClock() {
+    const now = new Date();
+    const options = { timeZone: currentTimezone, hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
+    const timeString = new Intl.DateTimeFormat('pt-BR', options).format(now);
+
+    const clock = document.getElementById('clock');
+    clock.textContent = timeString;
+}
+
+function setTimezone(timezone) {
+    currentTimezone = timezone;
+    updateClock();
+}
+
+setInterval(updateClock, 1000);
+updateClock(); // Initial call to display the clock immediately
